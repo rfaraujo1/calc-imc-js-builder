17e49ffe25683c1c8983a6941dc9b35de8cf04d3
Roberson Araujo <rfaraujo1@gmail.com>
2022-04-18 21:46:16 -0300

    build inicial (Atividade 3 Funções builder)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function Person(height, weight) {
-    if (typeof(height) !== 'number' || isNaN(height))
+    if (typeof (height) !== 'number' || isNaN(height))
         throw Error('height not a number');
 
-    if (typeof(weight) !== 'number' || isNaN(weight))
+    if (typeof (weight) !== 'number' || isNaN(weight))
         throw Error('weight not a number');
 
     this.height = height;
@@ -12,7 +12,7 @@ function Person(height, weight) {
 function Dietician(height, weight, crn) {
     Person.call(this, height, weight);
     this.crn = crn;
-    this.calculateImc = function() {
+    this.calculateImc = function () {
         return this.weight / Math.pow(this.height, 2);
     }
     console.log(this);
@@ -22,7 +22,7 @@ Dietician.prototype.constructor = Dietician;
 
 function Athlete(height, weight, crn) {
     Dietician.call(this, height, weight, crn);
-    this.calculateDiet = function() {
+    this.calculateDiet = function () {
         var imc = this.calculateImc();
         if (imc > 30) {
             return "Ultra leve";
@@ -35,16 +35,23 @@ Athlete.prototype = Object.create(Dietician);
 Athlete.prototype.constructor = Athlete;
 
 
-function calculateImc(alturaEl, pesoEl) {
-    var height = parseFloat(alturaEl.value);
-    var weight = parseFloat(pesoEl.value);
-    document.querySelector('#imc').innerHTML = new Dietician(height, weight, 1234).calculateImc();
-    document.querySelector('#diet').innerHTML = new Athlete(height, weight, 1234).calculateDiet();
-}
+function calculateImc() {
 
-window.onload = function(evt) {
-    var btn = document.querySelector(".form button");
     var alturaEl = document.querySelector('#altura');
     var pesoEl = document.querySelector('#peso');
-    btn.addEventListener("click", function() { calculateImc(alturaEl, pesoEl) });
+
+    return function () {
+        var height = parseFloat(alturaEl.value);
+        var weight = parseFloat(pesoEl.value);
+
+        document.querySelector('#imc').innerHTML = new Dietician(height, weight, 1234).calculateImc();
+        document.querySelector('#diet').innerHTML = new Athlete(height, weight, 1234).calculateDiet();
+    }
+}
+
+window.onload = function (evt) {
+    var btn = document.querySelector(".form button");
+
+    btn.addEventListener("click",
+        calculateImc());
 }
